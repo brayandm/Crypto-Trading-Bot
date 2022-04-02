a4dc1471758a5ad816fa5e4f51d3d574952a1204
FernanValdes <fvaldes0109@gmail.com>
2022-04-02 14:30:55 -0400

    Coded crypto purchase

--- models/constant.py
+++ models/constant.py
@@ -1,1 +1,5 @@
+import os
+
 EPSILON = 0.001
+WAIT_TIME = 120
+INCREMENT = int(os.environ['increment'])
--- models/worker.py
+++ models/worker.py
@@ -1,3 +1,4 @@
+import time
 from kucoin.client import Client
 
 from models import api, constant
@@ -5,16 +6,34 @@ from models import api, constant
 # Gets the currencies balances and determines if it's holding trade coin or not
 def evaluate_balance(client: Client, backup_currency: str, trading_currency: str):
 
+    time.sleep(10) # Give time to the DB to refresh and avoid double purchases
+
     back, trade = api.get_trade_balances(client, backup_currency, trading_currency)
 
+    # Not holding
     if trade < constant.EPSILON:
         check_past_prices(client, backup_currency, trading_currency)
+    else:
+        print('Preparing holding status')
 
 # Compares the past average with the current price and executes the corresponding action
 def check_past_prices(client: Client, backup_currency: str, trading_currency: str):
 
-        past_average = api.get_past_average(client, backup_currency, trading_currency)
-        current_price = api.get_current_price(client, backup_currency, trading_currency)
-        
-        print(past_average, current_price)
+        while True:
 
+            past_average = api.get_past_average(client, backup_currency, trading_currency)
+            current_price = api.get_current_price(client, backup_currency, trading_currency)
+
+            if past_average < current_price:
+                time.sleep(constant.WAIT_TIME)
+            else:
+                symbol = trading_currency + '-' + backup_currency
+                back, trade = api.get_trade_balances(client, backup_currency, trading_currency)
+                funds = round(back * 2 / 3, constant.INCREMENT)
+
+                client.create_market_order(symbol, 'buy', funds = funds)
+                print('Successfully invested ' + str(funds) + ' ' + backup_currency)
+                break
+
+        evaluate_balance(client, backup_currency, trading_currency)
+        
