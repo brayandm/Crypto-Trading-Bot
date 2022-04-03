b17e4904052d323f75458ce055ad6108199c3006
FernanValdes <fvaldes0109@gmail.com>
2022-04-02 23:28:41 -0400

    Selling bugfix

--- models/worker.py
+++ models/worker.py
@@ -55,7 +55,7 @@ def holding(client: Client, backup_currency: str, trading_currency: str):
 
         if current_price > upper_limit:
             back, trade = api.get_trade_balances(client, backup_currency, trading_currency)
-            client.create_limit_order(symbol, 'sell', upper_limit, trade)
+            client.create_limit_order(symbol, 'sell', round(upper_limit, constant.INCREMENT), trade)
             print('Take Profit: Successfully sold ' + str(trade) + ' ' + trading_currency)
             break
 
