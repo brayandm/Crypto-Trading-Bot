589d09d061e57c559faa90e2a119868dc8f2f20e
BrayanD <brayanduranmedina@gmail.com>
2022-04-12 01:25:01 -0400

    wallet2 added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import threading
 import os
 
 from exchanges.kucoin import Kucoin
+from exchanges.kucoin_virtual import KucoinVirtual
 from bots.bot import Bot
 from app_telegram import telegram_bot
 from app_telegram_commands import TelegramCommands
@@ -12,8 +13,9 @@ api_passphrase = os.environ['api_passphrase']
 is_sandbox = True if os.environ['is_sandbox'] == 'yes' else False
 
 wallet1 = Kucoin('KcWallet', api_key, api_secret, api_passphrase, is_sandbox)
+wallet2 = KucoinVirtual('VirtualWallet')
 bot1 = Bot('Robot', wallet1)
-telegram_commands = TelegramCommands(bot1, wallet1)
+telegram_commands = TelegramCommands(bot1, wallet1, wallet2)
 
 thread1 = threading.Thread(target=bot1.start_bot)
 thread2 = threading.Thread(target=telegram_commands.listen)
