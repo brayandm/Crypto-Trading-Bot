7e8d7d9b29dcf51b82e9b4e4a12607e54c1a2f28
BrayanD <brayanduranmedina@gmail.com>
2022-04-12 04:19:53 -0400

    rebooting system test

---
+++ app_main.py
@@ -0,0 +1,29 @@
+import threading
+import os
+
+from exchanges.app_kucoin import Kucoin
+from exchanges.app_kucoin_virtual import KucoinVirtual
+from bots.app_bot import Bot
+from app_telegram import telegram_bot
+from app_telegram_commands import TelegramCommands
+
+telegram_bot.send('Starting system')
+
+api_key = os.environ['api_key']
+api_secret = os.environ['api_secret']
+api_passphrase = os.environ['api_passphrase']
+is_sandbox = True if os.environ['is_sandbox'] == 'yes' else False
+
+wallet1 = Kucoin('KcWallet', api_key, api_secret, api_passphrase, is_sandbox)
+wallet2 = KucoinVirtual('VirtualWallet')
+bot1 = Bot('KcBot', wallet1)
+telegram_commands = TelegramCommands(bot1, wallet1, wallet2)
+
+thread1 = threading.Thread(target=bot1.start_bot)
+thread2 = threading.Thread(target=telegram_commands.listen)
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
